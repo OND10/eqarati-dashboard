dd99862380ed1dd8136f4398df9266708bbc30a9
OND10 <osamadammag84@gmail.com>
2025-08-21 10:59:09 +0300

    featL: add url of api

--- src/services/api/common/api-url.ts
+++ src/services/api/common/api-url.ts
@@ -1,9 +1,8 @@
-
-const APP_URL = "http://127.0.0.1:8000/";
+const APP_URL = 'https://instadar14-001-site1.Stempurl.com/API/';
 export default APP_URL;
 export function imagesUrl(trailing: string): string {
-  if (!trailing) return "/assets/imgs/User.png";
-  if (trailing.at(0) == "/") {
+  if (!trailing) return '/assets/imgs/User.png';
+  if (trailing.at(0) == '/') {
     trailing = trailing.slice(1);
   }
   return APP_URL + trailing;
